053aff77a7d1b8596ec3d72b02c414203bb3cd62
Jorge Valdivia <jvaldivia@rarestep.com>
2015-06-04 09:22:17 -0500

    added env

--- bin/killer.rb
+++ bin/killer.rb
@@ -1,9 +1,12 @@
-require "heroku_dyno_killer"
+require "bundler"
+Bundler.require
 
 killer = HerokuDynoKiller.new(
   {token: ENV["PAPERTRAIL_TOKEN"]},
   {app_name: ENV["APP_NAME"], token: ENV["HEROKU_TOKEN"]},
-  ENV["MEMORY_THRESHOLD_IN_MB"])
+  ENV["MEMORY_THRESHOLD_IN_MB"].to_f)
+
+puts killer.dynos_over_threshold
 
 # Restart all dynos that are over threshold. Returns dynos that were restarted.
-killer.restart
+# killer.restart
