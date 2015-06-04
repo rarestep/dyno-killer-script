316dec706b87aea0893cfb7435e3da955045ce6b
Jorge Valdivia <jvaldivia@rarestep.com>
2015-06-04 09:51:26 -0500

    more log tweaking

--- bin/killer.rb
+++ bin/killer.rb
@@ -1,13 +1,14 @@
 require "bundler"
 Bundler.require
 
+$stdout.sync = true
+
 killer = HerokuDynoKiller.new(
   {token: ENV["PAPERTRAIL_TOKEN"]},
   {app_name: ENV["APP_NAME"], token: ENV["HEROKU_TOKEN"]},
   ENV["MEMORY_THRESHOLD_IN_MB"].to_f)
 
-$stdout.print killer.dynos_over_threshold
-$stdout.flush
+puts killer.dynos_over_threshold
 
 # Restart all dynos that are over threshold. Returns dynos that were restarted.
 # killer.restart
