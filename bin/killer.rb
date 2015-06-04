2cd9a277f499d517d3c60a998023d655c33bc95b
Jorge Valdivia <jvaldivia@rarestep.com>
2015-06-04 09:29:04 -0500

    stdout

--- bin/killer.rb
+++ bin/killer.rb
@@ -6,7 +6,8 @@ killer = HerokuDynoKiller.new(
   {app_name: ENV["APP_NAME"], token: ENV["HEROKU_TOKEN"]},
   ENV["MEMORY_THRESHOLD_IN_MB"].to_f)
 
-puts killer.dynos_over_threshold
+$stdout.print killer.dynos_over_threshold
+$stdout.flush
 
 # Restart all dynos that are over threshold. Returns dynos that were restarted.
 # killer.restart
